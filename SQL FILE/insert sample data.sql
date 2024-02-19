e7fd3d422e38956d33c6b2ad5a35a351c2160689
Marina Fouad <108586666+MarinaFouad@users.noreply.github.com>
2024-02-19 18:44:33 +0200

    Add files via upload

---
+++ SQL FILE/insert sample data.sql
@@ -0,0 +1,43 @@
+use AcademicCourseDB;
+go
+insert into Accounts values
+('admin','1','Admin'),
+('teacher1','1','Teacher'),
+('teacher2','1','Teacher'),
+('teacher3','1','Teacher'),
+('student1','1','Student'),
+('student2','1','Student'),
+('student3','1','Student'),
+('student4','1','Student'),
+('student5','1','Student')
+
+go
+insert into Track values
+('preparatory school grade 1 English'),
+('preparatory school grade 1 Arabic'),
+('preparatory school grade 2 English'),
+('preparatory school grade 2 Arabic'),
+('preparatory school grade 3 English'),
+('preparatory school grade 3 Arabic')
+
+go
+insert into Teacher values
+('teacher1','0111111111','this is the adress',DEFAULT,2),
+('teacher2','0111111111','this is the adress',DEFAULT,3),
+('teacher3','0111111111','this is the adress',DEFAULT,4)
+
+go
+insert into Student values
+('student1','0111111111','this is the adress',DEFAULT,5),
+('student2','0111111111','this is the adress',DEFAULT,6),
+('student3','0111111111','this is the adress',DEFAULT,7),
+('student4','0111111111','this is the adress',DEFAULT,8),
+('student5','0111111111','this is the adress',DEFAULT,9)
+
+
+
+
+
+
+
+
